3bb291b4e69fe1c20a80dae30208cd995190eea8
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-07-17 16:43:06 +0700

    Add movemet with keyboard

---
+++ Assets/Script/MoveWithoutVR.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithoutVR : MonoBehaviour
+{
+  #region movementControl
+  public float moveSpeed = 1f;
+  #endregion
+  #region CameraSetting
+  float rotationX = 0f;
+  float rotationY = 0f;
+  [SerializeField] float cameraSentivity = 15f;
+  #endregion
+
+  Vector3 move;
+
+  void Start()
+  {
+
+  }
+  void FixedUpdate()
+  {
+    movement();
+    cameraMoveWithMouse();
+  }
+
+  void movement()
+  {
+    float moveX = Input.GetAxisRaw("Horizontal");
+    float moveY = Input.GetAxisRaw("Vertical");
+    move = new Vector3(moveX, 0f, moveY);
+    move = move * moveSpeed * Time.deltaTime;
+    transform.position += move;
+  }
+
+  void cameraMoveWithMouse()
+  {
+    float cameraMoveX = Input.GetAxisRaw("Mouse Y");
+    float cameraMoveY = Input.GetAxisRaw("Mouse X");
+    rotationX += cameraMoveX * -1 * cameraSentivity * Time.deltaTime;
+    rotationY += cameraMoveY * cameraSentivity * Time.deltaTime;
+    transform.localEulerAngles = new Vector3(rotationX, rotationY, 0f);
+
+
+  }
+}
