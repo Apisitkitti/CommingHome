538a792932fc3f390a12d5d2c95eef8383ec9cfc
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2024-08-01 21:39:38 +0700

    Edit:Spawn Car

--- Assets/Script/Car/CarSpawn.cs
+++ Assets/Script/Car/CarSpawn.cs
@@ -6,69 +6,79 @@ using UnityEngine;
 
 public class CarSpawn : MonoBehaviour
 {
-  [Header("Spawn")]
-  [SerializeField] List<Transform> carSpawner;
-  [SerializeField] List<GameObject> spawnerGameObject;
-  [SerializeField] GameObject carPrefab;
-  [SerializeField] List<GameObject> carThatSpawn;
-  int spawnerNumber = 0;
+    [Header("Spawn")]
+    [SerializeField] List<Transform> carSpawner;
+    [SerializeField] List<GameObject> spawnerGameObject;
+    [SerializeField] GameObject carPrefab;
+    [SerializeField] List<GameObject> carThatSpawn;
+    int spawnerNumber = 0;
+    bool isSpawning = false;
 
-  #region ForSetSpawnCar
-  [Header("CarSpawnSetting")]
-  [SerializeField] float spawnTimeSetting = 0f;
-  public List<bool> isSpawn;
+    #region ForSetSpawnCar
+    [Header("CarSpawnSetting")]
+    [SerializeField] float spawnTimeSetting = 0f;
+    public List<bool> isSpawn;
 
-  #endregion
-  void Awake()
-  {
-    foreach (Transform spawnerTransform in carSpawner)
+    #endregion
+
+    void Awake()
     {
-      spawnerGameObject.Add(spawnerTransform.gameObject);
+        foreach (Transform spawnerTransform in carSpawner)
+        {
+            spawnerGameObject.Add(spawnerTransform.gameObject);
+        }
     }
-  }
-  void Start()
-  {
-    spawnObjectSet(false);
-  }
-  void Update()
-  {
-    checkWhereToSpawn();
-  }
 
-  public void checkWhereToSpawn()
-  {
-    foreach (bool isSpawnRequest in isSpawn)
+    void Start()
     {
-      if (isSpawnRequest)
-      {
-        spawnerGameObject[spawnerNumber].SetActive(true);
-        StartCoroutine(carSpawn());
-        spawnerNumber++;
-      }
-      if (spawnerNumber >= carSpawner.Count)
-      {
-        spawnerNumber = 0;
-      }
+        spawnObjectSet(false);
     }
-  }
-  IEnumerator carSpawn()
-  {
-    carSpawnAndAddList(spawnerNumber);
-    yield return new WaitForSeconds(spawnTimeSetting);
-  }
-  void carSpawnAndAddList(int spawnerNumber)
-  {
-    GameObject carSpawn = Instantiate(carPrefab, carSpawner[spawnerNumber].position, carSpawner[spawnerNumber].rotation);
-    carThatSpawn.Add(carSpawn);
-  }
 
-  void spawnObjectSet(bool setBool)
-  {
-    for (int spawnCheck = 0; spawnCheck < isSpawn.Count; spawnCheck++)
+    void Update()
     {
-      isSpawn[spawnCheck] = setBool;
-      spawnerGameObject[spawnCheck].SetActive(setBool);
+        checkWhereToSpawn();
     }
-  }
+
+    public void checkWhereToSpawn()
+    {
+        if (!isSpawning)
+        {
+            foreach (bool isSpawnRequest in isSpawn)
+            {
+                if (isSpawnRequest)
+                {
+                    spawnerGameObject[spawnerNumber].SetActive(true);
+                    StartCoroutine(carSpawn());
+                    spawnerNumber++;
+                    if (spawnerNumber >= carSpawner.Count)
+                    {
+                        spawnerNumber = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator carSpawn()
+    {
+        isSpawning = true;
+        carSpawnAndAddList(spawnerNumber);
+        yield return new WaitForSeconds(spawnTimeSetting);
+        isSpawning = false;
+    }
+
+    void carSpawnAndAddList(int spawnerNumber)
+    {
+        GameObject carSpawn = Instantiate(carPrefab, carSpawner[spawnerNumber].position, carSpawner[spawnerNumber].rotation);
+        carThatSpawn.Add(carSpawn);
+    }
+
+    void spawnObjectSet(bool setBool)
+    {
+        for (int spawnCheck = 0; spawnCheck < isSpawn.Count; spawnCheck++)
+        {
+            isSpawn[spawnCheck] = setBool;
+            spawnerGameObject[spawnCheck].SetActive(setBool);
+        }
+    }
 }
-
--- Assets/Script/object/TrafficSignManager.cs
+++ Assets/Script/object/TrafficSignManager.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 
 public class TrafficSignManager : MonoBehaviour
 {
-  [SerializeField] List<MeshRenderer> lightBulb;
-  [SerializeField] List<Material> color;
-  [SerializeField] CarSpawn carSpawn;
-  public int changeLightRedToYellow;
-  public int changeLightYellowToGreen;
-  public int changeLightGreenToRed;
-  void Start()
-  {
-    lightBulb[0].material = color[1];
-    StartCoroutine(lightChanger());
-  }
-  IEnumerator lightChanger()
-  {
-    while (true)
+    [SerializeField] List<MeshRenderer> lightBulb;
+    [SerializeField] List<Material> color;
+    [SerializeField] CarSpawn carSpawn;
+    public int changeLightRedToYellow;
+    public int changeLightYellowToGreen;
+    public int changeLightGreenToRed;
+
+    void Start()
     {
-      carSpawn.isSpawn.Clear();
-      lightBulb[0].material = color[1];
-      lightBulb[2].material = color[0];
-      carSpawn.isSpawn.Add(false);
-      yield return new WaitForSeconds(changeLightRedToYellow);
-      lightBulb[0].material = color[0];
-      lightBulb[1].material = color[2];
-      yield return new WaitForSeconds(changeLightYellowToGreen);
-      lightBulb[1].material = color[0];
-      lightBulb[2].material = color[3];
-      carSpawn.isSpawn.Add(true);
-      yield return new WaitForSeconds(changeLightGreenToRed);
+        lightBulb[0].material = color[1];
+        StartCoroutine(lightChanger());
     }
-  }
 
+    IEnumerator lightChanger()
+    {
+        while (true)
+        {
+            carSpawn.isSpawn.Clear();
+            carSpawn.isSpawn.Add(false);
+            lightBulb[0].material = color[1];
+            lightBulb[2].material = color[0];
+            yield return new WaitForSeconds(changeLightRedToYellow);
 
+            lightBulb[0].material = color[0];
+            lightBulb[1].material = color[2];
+            yield return new WaitForSeconds(changeLightYellowToGreen);
+
+            lightBulb[1].material = color[0];
+            lightBulb[2].material = color[3];
+           // carSpawn.isSpawn.Clear();
+            carSpawn.isSpawn.Add(true);
+            yield return new WaitForSeconds(changeLightGreenToRed);
+        }
+    }
 }
+
