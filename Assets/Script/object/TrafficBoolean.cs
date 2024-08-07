a14a31ecbdb318fc5db856dc350e26008d7d2bd6
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-08-07 21:21:23 +0700

    update website

--- Assets/Script/object/TrafficBoolean.cs
+++ Assets/Script/object/TrafficBoolean.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 public class TrafficBoolean : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
