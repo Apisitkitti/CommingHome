e64331eeb9338f8551b993242140d11fc4d4b7be
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-08-13 17:17:23 +0700

    adapt code for death image in future

--- Assets/Script/Story/DeathStory.cs
+++ Assets/Script/Story/DeathStory.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DeathStory : MonoBehaviour
 {
   [Header("TimeForWordSetting")]
   [SerializeField] int timeBetweenWord;
   [SerializeField] int wordAppear;
-  [Header("Message")]
+  [Header("MessageAndImage")]
   [SerializeField] List<string> deathMessage;
+  [SerializeField] List<Sprite> deathImageBackground;
 
   [Header("Input Object")]
   [SerializeField] TMP_Text deathText;
+  [SerializeField] Image image;
   [SerializeField] DeathStorySet deathStorySet;
   public int deathmessageNumber = 0;
 
@@ -45,9 +48,10 @@ public class DeathStory : MonoBehaviour
       yield return new WaitForSeconds(timeBetweenWord);
     }
     yield return new WaitForSeconds(wordAppear);
+    image.sprite = deathImageBackground[deathmessageNumber];
     resetDeathDialogueState(false);
   }
-  void resetDeathDialogueState(bool active)
+  public void resetDeathDialogueState(bool active)
   {
     deathText.text = string.Empty;
 
