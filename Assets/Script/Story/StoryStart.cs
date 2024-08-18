d81ec35236aef087ea033c4984da01654975b12f
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-08-18 22:32:39 +0700

    text at start

--- Assets/Script/Story/StoryStart.cs
+++ Assets/Script/Story/StoryStart.cs
@@ -6,26 +6,41 @@ using TMPro;
 public class StoryStart : MonoBehaviour
 {
   [SerializeField] TMP_Text storyText;
-  [SerializeField] GameObject checkPoint;
-  [SerializeField] string lines;
+  [SerializeField] List<string> lines;
   [SerializeField] float textSpeed;
+  [SerializeField] float uiDisappear;
+  [SerializeField] DeathStorySet normalStory;
+  [SerializeField] GameObject storyUi;
+  int dialogueNumber;
 
   void Start()
   {
     storyText.text = string.Empty;
+    storyUi.SetActive(false);
+  }
+
+  void storySetter()
+  {
+    foreach (bool storyIsCheck in normalStory.storyCheck)
+    {
+      if (storyIsCheck) dialogueNumber++;
+    }
   }
   public void startDialogue()
   {
+    storySetter();
+    storyUi.SetActive(true);
     StartCoroutine(typeLine());
-    Destroy(checkPoint);
 
   }
   IEnumerator typeLine()
   {
-    foreach (char charcterInConversation in lines)
+    foreach (char charcterInConversation in lines[dialogueNumber])
     {
       storyText.text += charcterInConversation;
       yield return new WaitForSeconds(textSpeed);
     }
+    yield return new WaitForSeconds(uiDisappear);
+    storyUi.SetActive(false);
   }
 }
