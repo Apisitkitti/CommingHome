bf563d424f739e41d6555323c9a6468dc4fb0aca
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-10-21 23:27:33 +0700

    car turn but unsuccessful it not turn and use tornadodance

--- Assets/Script/Car/CarMove.cs
+++ Assets/Script/Car/CarMove.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
   public float carSpeed = 0f;
+  public float turnSpeed;
   [SerializeField] Rigidbody rb;
   private Vector3 initForward;
+  private float rotationAngle;
+
 
   void Start()
   {
     rb = GetComponent<Rigidbody>();
     initForward = transform.forward;
+    rotationAngle = transform.rotation.y;
   }
   void Update()
   {
-    rb.velocity = initForward * carSpeed * Time.deltaTime;
-
+    carForward();
+    carTurn();
   }
 
   void OnCollisionEnter(Collision col)
@@ -31,4 +36,38 @@ public class CarMove : MonoBehaviour
       Destroy(gameObject);
     }
   }
+  void OnTriggerEnter(Collider col)
+  {
+    if (col.gameObject.tag == "leftTurn")
+    {
+      StartCoroutine(turnLeft());
+      Destroy(col.gameObject);
+    }
+    if (col.gameObject.tag == "rightTurn")
+    {
+      StartCoroutine(turnRight());
+      Destroy(col.gameObject);
+    }
+  }
+  void carTurn()
+  {
+    transform.Rotate(0, rotationAngle, 0);
+  }
+  IEnumerator turnLeft()
+  {
+    while (rotationAngle % 90 != 0)
+      yield return rotationAngle -= turnSpeed;
+
+  }
+  IEnumerator turnRight()
+  {
+    while (rotationAngle % 90 != 0)
+      yield return rotationAngle += turnSpeed;
+  }
+  void carForward()
+  {
+    rb.velocity = initForward * carSpeed * Time.deltaTime;
+  }
 }
+
+
