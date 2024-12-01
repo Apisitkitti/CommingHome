d87c4a724d7072ffa2b726159a0d74f53ebfb252
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2024-12-01 15:35:03 +0700

    Add:Animation Run Scavenger

--- Assets/Script/enemy/MoveAlongSpline.cs
+++ Assets/Script/enemy/MoveAlongSpline.cs
@@ -8,8 +8,13 @@ public class MoveAlongSpline : MonoBehaviour
     [SerializeField] private SplineContainer splineContainer; // ตัว Spline ที่ต้องการให้เดินตาม
     [SerializeField] private float speed = 1f; // ความเร็วเริ่มต้นในการเคลื่อนที่
     [SerializeField] private float speedIncrease = 5f;
+    private Animator animator;
     private float progress = 0f; // ตำแหน่งปัจจุบันบน Spline (0-1)
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         if (splineContainer == null) return;
@@ -18,21 +23,24 @@ public class MoveAlongSpline : MonoBehaviour
         progress += speed * Time.deltaTime / splineContainer.Spline.GetLength();
 
         // Reset progress เมื่อเกิน 1 (วนลูป)
-        if (progress > 1f)
-            progress -= 1f;
+        // if (progress > 1f)
+        //     progress -= 1f;
 
         // คำนวณตำแหน่งบน Spline
         Vector3 position = splineContainer.EvaluatePosition(progress);
         transform.position = position;
-
+       
         // คำนวณการหมุนเพื่อให้หันตามทิศทาง
         Vector3 direction = splineContainer.EvaluateTangent(progress);
-        if (direction != Vector3.zero)
+        if (direction != Vector3.zero){
             transform.rotation = Quaternion.LookRotation(direction);
+        }
+
     }
 
     public void ScavengerHuntMode(){
         speed = speedIncrease;
+        animator.SetBool("IsRunning",true);
     }
 }
 
