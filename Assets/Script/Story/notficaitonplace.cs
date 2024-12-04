a1813573f713aaffa58a756afc1c514c9a04aa28
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2024-12-04 17:39:24 +0700

    Add:Sound Notication line Every Scene/Ambient Sound Add:CheckNumber Scriptable 16,17

--- Assets/Script/Story/notficaitonplace.cs
+++ Assets/Script/Story/notficaitonplace.cs
@@ -7,6 +7,8 @@ public class notficaitonplace : MonoBehaviour
   [SerializeField] int storySendNumber;
   [SerializeField] notification setNoti;
   [SerializeField] GameObject uiNoti;
+  [SerializeField] AudioSource audioSource;
+  [SerializeField] private bool PlaySoundOneTime;
   public int notiNumberForShow;
 
   void OnTriggerEnter(Collider playerTag)
@@ -14,6 +16,7 @@ public class notficaitonplace : MonoBehaviour
     if (playerTag.gameObject.tag == "Player")
     {
       StartCoroutine(delayNoti());
+      StartSoundNotiLine();
     }
   }
   IEnumerator delayNoti()
@@ -22,5 +25,15 @@ public class notficaitonplace : MonoBehaviour
     yield return new WaitForSeconds(notiNumberForShow);
     uiNoti.SetActive(false);
     Destroy(gameObject);
+  }
+
+  private bool StartSoundNotiLine()
+  {
+    if(PlaySoundOneTime == false)
+    {
+      audioSource.Play();
+      PlaySoundOneTime = true;
+    }
+    return PlaySoundOneTime;
   }
 }
